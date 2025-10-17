8872591a7def7bbd91a9e82f84be21024298ffe4
William Tremblay <tremwil@gmail.com>
2025-10-17 12:40:02 +0100

    only rely on OS APIs when necessary

---
+++ src/os.rs
@@ -0,0 +1,9 @@
+//! Implementations of os_arch_clear_cache for architectures where it is a privileged
+//! operation that requires calling into the operating system.
+
+#[cfg_attr(any(target_os = "linux", target_os = "android"), path = "linux.rs")]
+#[cfg_attr(target_os = "windows", path = "windows.rs")]
+#[cfg_attr(target_os = "macos", path = "macos.rs")]
+mod clear_cache_impl;
+
+pub(crate) use clear_cache_impl::os_arch_clear_cache;
